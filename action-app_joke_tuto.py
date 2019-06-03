cccd1328633f185848cc6bf5a99e3b1d74624741
toby2018 <36097390+toby2018@users.noreply.github.com>
2019-06-03 19:34:58 +0200

    commit

--- action-app_joke_tuto.py
+++ action-app_joke_tuto.py
@@ -5,6 +5,7 @@ from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import requests
 
 CONFIG_INI = "config.ini"
 
@@ -15,7 +16,7 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-class Template(object):
+class JokeTuto(object):
     """Class used to wrap action code with mqtt connection
         
         Please change the name refering to your application
@@ -32,36 +33,37 @@ class Template(object):
         self.start_blocking()
         
     # --> Sub callback function, one per intent
-    def intent_1_callback(self, hermes, intent_message):
-        # terminate the session first if not continue
-        hermes.publish_end_session(intent_message.session_id, "")
-        
-        # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
-
-        # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Action1 has been done", "")
-
-    def intent_2_callback(self, hermes, intent_message):
+    def askJoke_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+        good_category = requests.get("https://api.chucknorris.io/jokes/categories").json();
+
+        category = None
+        if intent_message.slots.category:
+            category = intent_message.slots.category.first().value
+            # check if the category is valide
+            if category.encode("utf-8") not in good_category:
+                category = None
+
+        if category is None:
+            joke_msg = str(requests.get("https://api.chucknorris.io/jokes/random") \
+                           .json().get("value"))
+        else:
+            joke_msg = str(requests.get("https://api.chucknorris.io/jokes/random?category={}".format(category)) \
+                           .json().get("value"))
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Action2 has been done", "")
-
+        hermes.publish_start_session_notification(intent_message.site_id, joke_msg, "Joke_Tuto_APP")
+    
     # More callback function goes here...
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
-        if coming_intent == 'intent_1':
-            self.intent_1_callback(hermes, intent_message)
-        if coming_intent == 'intent_2':
-            self.intent_2_callback(hermes, intent_message)
-
+        if coming_intent == 'toby2018:askJoke':
+            self.askJoke_callback(hermes, intent_message)
         # more callback and if condition goes here...
 
     # --> Register callback function and start MQTT
@@ -69,5 +71,6 @@ class Template(object):
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intents(self.master_intent_callback).start()
 
+
 if __name__ == "__main__":
-    Template()
+    JokeTuto()
